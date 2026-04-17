3f32d851cc8c8d5381ea1ea1cbf4589eaf4eb25e
Fazil9847 <fazil984799@gmail.com>
2026-04-18 00:43:09 +0530

    pwa removed we will add later

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,38 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      devOptions: {
-        enabled: true
-      },
-      manifest: {
-        name: 'Homly Deals',
-        short_name: 'Homly',
-        description: 'Premium Furniture Store',
-        theme_color: '#000000',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        scope: '/',
-        icons: [
-          {
-            src: '/logo192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/logo512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ]
+  plugins: [react()]
 })
